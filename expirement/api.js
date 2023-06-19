551b7967892b13f1832c96c62d66c9563393fbb2
Nilkh <khsrawynilofar@gmail.com>
2023-06-19 11:02:13 -0600

    add api.js and using promises

--- expirement/api.js
+++ expirement/api.js
@@ -1,1 +1,9 @@
 //your code
+const URL =
+	'https://www.boredapi.com/api/activity?minprice=0&maxprice=0.1&participants%3E1'
+
+fetch(URL).then(res => {
+    console.log(res)
+
+})
+// console.log(fetch(URL))
